8c7db35414722eaec43b23caa872fcd12bf3a4c1
Acalltoredemption <acalltoredemption@gmail.com>
2022-01-24 17:18:50 -0800

    card component pulling data from robots

---
+++ src/robots.js
@@ -0,0 +1,71 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Joseph Stalin',
+        username: 'Tony',
+        email: 'cool@ok.com'
+
+    },
+    {
+        id: 2,
+        name: 'Robbie Oren',
+        username: 'Robbie',
+        email: 'robbie@ok.com'
+
+    },
+    {
+        id: 3,
+        name: 'Donald Trump',
+        username: 'Dtrump1',
+        email: 'dtrump@ok.com'
+
+    },
+    {
+        id: 4,
+        name: 'Neil Armstrong',
+        username: 'Moonman',
+        email: 'neil@nasa.gov'
+
+    },
+    {
+        id: 5,
+        name: 'Felix',
+        username: 'Pewdiepie',
+        email: 'felix@yahoo.com'
+    },
+    {
+        id: 6,
+        name: 'Jay Jay',
+        username: 'JJ',
+        email: 'jj@cocomelon.com'
+
+    },
+    {
+        id: 7,
+        name: 'Shredder',
+        username: 'Sh123',
+        email: '123@ok.com'
+
+    },
+    {
+        id: 8,
+        name: 'Anonymous',
+        username: 'Mysterio',
+        email: 'mysterio@mystery.com'
+
+    },
+    {
+        id: 9,
+        name: 'Frank Sinatra',
+        username: 'Shark',
+        email: 'wow@gmail.com'
+
+    },
+    {
+        id: 10,
+        name: 'Joey Diaz',
+        username: 'CoolGuy',
+        email: 'cool2@ok.com'
+
+    }
+]
