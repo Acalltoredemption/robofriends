703566907bbf3dbfa7b80a53705308e6e970b840
Acalltoredemption <acalltoredemption@gmail.com>
2022-01-26 18:14:50 -0800

    error catching for cardlist with errorboundry

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './Searchbox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/Searchbox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 
@@ -27,14 +29,19 @@ class App extends Component {
     }
 
     render(){
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robots => {
+            return robots.name.toLowerCase().includes(searchfield.toLowerCase())
         })
         return (
             <div className="tc">
             <h1 className="f1">RoboFriends</h1>
             <SearchBox searchChange={this.onSearchChange} />
+            <Scroll>
+                <ErrorBoundry>
             <CardList robots={filteredRobots} />
+                </ErrorBoundry>
+            </Scroll>
             </div>
         );
     }
