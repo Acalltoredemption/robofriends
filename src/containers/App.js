3fb0ca53672fd9b05c04342501d5bbc2ce7bf09f
Acalltoredemption <acalltoredemption@gmail.com>
2022-01-30 16:57:09 -0800

    refactoring app to use hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/Searchbox';
 import Scroll from '../components/Scroll';
@@ -7,36 +7,44 @@ import './App.css';
 
 
 
-class App extends Component {
-    constructor(){
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
+function App () {
+    // constructor(){
+    //     super()
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: ''
+    //     }
+    // }
+
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
+
+    // componentDidMount(){
+    //     fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+    //         return response.json();
+    //     }).then(users => {
+    //         this.setState({robots: users})
+    //     })
+    // }
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => setRobots(users));
+    }, []);
+
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);
     }
 
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users').then(response => {
-            return response.json();
-        }).then(users => {
-            this.setState({robots: users})
-        })
-    }
-
-    onSearchChange = (event) => {
-        this.setState({searchfield : event.target.value});
-    }
-
-    render(){
-        const {robots, searchfield} = this.state;
+    
         const filteredRobots = robots.filter(robots => {
             return robots.name.toLowerCase().includes(searchfield.toLowerCase())
         })
         return (
             <div className="tc">
             <h1 className="f1">RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange} />
+            <SearchBox searchChange={onSearchChange} />
             <Scroll>
                 <ErrorBoundry>
             <CardList robots={filteredRobots} />
@@ -45,6 +53,6 @@ class App extends Component {
             </div>
         );
     }
-}
+
 
 export default App;
